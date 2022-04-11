a3e1a3d94ad001151f26a68f78179cce7f7176d3
Millana <chotubaevam@gmail.com>
2022-04-11 14:18:55 +0600

    message deliver

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
 function App() {
+  const [message, setMessage] = useState("");
+  const [messageToSend, setMessageToSend] = useState("");
+  const inputButton = () => {
+    setMessageToSend(message);
+    setMessage("")
+  };
+  const valueInput = (e) => {
+    e.preventDefault();
+    setMessage(e.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-box">
+      <h1>A Message You Would Like To Pass</h1>
+      <input value={message} onChange={(e) => valueInput(e)} />
+      <div className="messageToSent">
+        <button className="btn" onClick={() => inputButton()}>
+          Оправить
+        </button>
+        <h1>Last Message Delivered</h1>
+        <h3>{messageToSend}</h3>
+      </div>
     </div>
   );
 }
